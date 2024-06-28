a2f6f36535c268cee8b94bc8e0772c8d665ad6fc
Nishad <avinash.nishad@capgemini.com>
2024-06-29 04:02:58 +0530

    code updated

--- java/spring-framework/build.gradle.kts
+++ java/spring-framework/build.gradle.kts
@@ -14,6 +14,8 @@ dependencies {
     implementation("org.springframework:spring-context:6.1.10")
     implementation("org.springframework:spring-core:6.1.10")
     implementation("org.springframework:spring-aop:6.1.10")
+    implementation("org.aspectj:aspectjtools:1.9.22.1")
+    runtimeOnly("org.aspectj:aspectjrt:1.9.22.1")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
